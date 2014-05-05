c09c4171021e1854108a6d6391487fc6a0d379e8
Tobias Schottdorf <tobias.schottdorf@gmail.com>
2014-05-04 18:55:51 -0700

    finished #72

--- 72_channels.go
+++ 72_channels.go
@@ -6,12 +6,27 @@ import (
 	"code.google.com/p/go-tour/tree"
 )
 
+func DoWalk(t1, t2 *tree.Tree, ch1, ch2 chan int, done chan bool) {
+	fmt.Println("Walking")
+	go func() {
+		Walk(t1, ch1)
+		close(ch1)
+	}()
+	go func() {
+		Walk(t2, ch2)
+		close(ch2)
+	}()
+	fmt.Println("Triggering control channel")
+	done <- true
+}
+
 // Walk walks the tree t sending all values
 // from the tree to the channel ch.
 func Walk(t *tree.Tree, ch chan int) {
 	if t.Left != nil {
 		Walk(t.Left, ch)
 	}
+	fmt.Println("Writing to channel")
 	ch <- t.Value
 	if t.Right != nil {
 		Walk(t.Right, ch)
@@ -23,24 +38,40 @@ func Walk(t *tree.Tree, ch chan int) {
 // t1 and t2 contain the same values.
 func Same(t1, t2 *tree.Tree) bool {
 	var ch1, ch2 = make(chan int), make(chan int)
-	go Walk(t1, ch1)
-	go Walk(t2, ch2)
+	var done = make(chan bool)
+	go DoWalk(t1, t2, ch1, ch2, done)
+	<-done
+
 	var b int
-	var ok bool
+	var ok bool = true
 	for a := range ch1 {
-		fmt.Println(a)
-		b, ok = <-ch2
-		if !ok || a != b {
+		fmt.Println("Read", a)
+		if !ok {
+			// b was done in the last iteration, but a has another item -> not equal
+			fmt.Println("Tree 1 has more elements")
 			return false
 		}
-
+		b, ok = <-ch2
+		if a != b {
+			fmt.Println("Different elements encountered")
+			return false
+		}
+	}
+	select {
+	case _, ok := <-ch2:
+		if ok {
+			fmt.Println("Tree 2 has more elements")
+			return false
+		}
 	}
 	return true
 
 }
 
 func main() {
-	if Same(tree.New(1), tree.New(1)) {
+	if Same(tree.New(2), tree.New(2)) {
 		fmt.Println("Good")
+	} else {
+		fmt.Println("Bad")
 	}
 }
